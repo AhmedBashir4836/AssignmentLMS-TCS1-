b87f59d08646b5eddb1addb64eb0b50472eb448a
ahmedbashir <sheikhahmed760@gmail.com>
2021-06-15 23:56:45 +0500

    Adding all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lms';
+  name = 'Ahmed Bashir';
+  testVar = 'Testing 2 way data binding';
+  save() {
+    alert('You Clicked')
+  }
 }
